ccb9aa163939a6cc078e621cf1049b227bc31651
erredev-JS <roddev100@gmail.com>
2025-03-24 20:24:04 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Aside } from "./components/ui/Aside/Aside"
 import { Header } from "./components/ui/Header/Header"
 import { ListBacklog } from "./components/ui/listBacklog/ListBacklog"
->>>>>>> 742955a34f01a8a053eb1a95567c7b381d6cc23d
+
 
 
 function App() {
