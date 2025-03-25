4bc8b35dd886bfa012326f24127b893f9389bbf1
erredev-JS <roddev100@gmail.com>
2025-03-24 22:06:32 -0300

    BacklogScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import styles from './components/screens/App.module.css'
-import { Aside } from "./components/ui/Aside/Aside"
-import { Header } from "./components/ui/Header/Header"
-import { ListBacklog } from "./components/ui/ListBacklog/ListBacklog"
 
+import { BacklogScreen } from './components/screens/BacklogScreen/BacklogScreen'
 
 
 
@@ -10,16 +7,7 @@ function App() {
 
   return (
     <>
-
-
-      <Header></Header>
-      <div className={styles.mainContainer}>
-
-      <Aside></Aside>
-      <ListBacklog></ListBacklog>
-        
-      </div>
-
+    <BacklogScreen></BacklogScreen>
     </>
   )
 }
---
+++ src/components/screens/BacklogScreen/BacklogScreen.tsx
@@ -0,0 +1,21 @@
+import { Aside } from "../../ui/Aside/Aside"
+import { Header } from "../../ui/Header/Header"
+import { ListBacklog } from "../../ui/ListBacklog/ListBacklog"
+
+
+import styles from './BacklogScreen.module.css'
+
+export const BacklogScreen = () => {
+  return (
+    
+    <>
+        <Header></Header>
+      <div className={styles.mainContainer}>
+
+      <Aside></Aside>
+      <ListBacklog></ListBacklog>
+        
+      </div>
+    </>
+  )
+}
