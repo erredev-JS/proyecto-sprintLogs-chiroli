a954fe0d8e0d40bbc587cfdc157b58e49fc500cd
erredev-JS <roddev100@gmail.com>
2025-03-28 14:19:28 -0300

    storeSprints

---
+++ src/store/useStoreSprints.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+import { ISprint } from "../types/ISprints";
+
+
+
+
+interface IStoreSprints {
+
+    sprintActiva: ISprint | null
+
+    sprints: ISprint[],
+
+    addSprint: (sprint: ISprint) => void
+    editSprint: (sprintActualizado: ISprint) => void
+    deleteSprint: (idSprint: string) => void
+
+  
+    setSprintActiva: (tarea: ISprint | null) => void
+   
+}
+
+
+export const useStoreSprints = create<IStoreSprints>((set) => ({
+   
+    sprintActiva: null,
+
+
+    sprints: [] as ISprint[],
+
+    addSprint: (sprint) => set((state) => ({
+        sprints: [...state.sprints, sprint]
+    })),
+    
+
+    editSprint: (sprintActualizada) => set((state) => {
+        const arraySprints =  state.sprints.map((sprint) => sprint.id === sprintActualizada.id ? {...sprint, ...sprintActualizada} : sprint)
+        return {sprints: arraySprints}
+    }),
+
+    deleteSprint: (idSprint) => set((state) => ({
+        sprints : state.sprints.filter((sprint) => sprint.id !== idSprint)
+
+    })),
+
+
+    
+    setSprintActiva: (sprintActivaIn) => set(() => ({ sprintActiva: sprintActivaIn}))
+}))
+
+
+export default useStoreSprints
