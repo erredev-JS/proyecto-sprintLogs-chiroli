eacebffcf98895934cdc39b31e027ca6378356b5
erredev-JS <roddev100@gmail.com>
2025-03-29 17:20:11 -0300

    setStateTarea

--- src/store/useStoreTareas.ts
+++ src/store/useStoreTareas.ts
@@ -13,6 +13,7 @@ interface IStoreTareas {
     editTarea: (tareaActualizada: ITareas) => void
     deleteTarea: (idTarea: string) => void
 
+    setTareaState: (idTarea: string, state: string) => void
 
     setTareaActiva: (tarea: ITareas | null) => void
     setTareas: (tareas: ITareas[]) => void
@@ -29,10 +30,18 @@ export const useStoreTareas = create<IStoreTareas>((set) => ({
         tareas: [...state.tareas, tarea]
     })),
     editTarea: (tareaActualizada) => set((state) => {
-        const tareasInactivas =  state.tareas.map((tarea) => tarea.id === tareaActualizada.id ? {...tarea, ...tareaActualizada} : tarea)
-        return {tareas: tareasInactivas}
+        const tareas =  state.tareas.map((tarea) => tarea.id === tareaActualizada.id ? {...tarea, ...tareaActualizada} : tarea)
+        return {tareas: tareas}
     }),
 
+    setTareaState: (idTarea, nuevoEstado) => set((state) => ({
+        tareas: state.tareas.map((tarea) => 
+            tarea.id === idTarea ? { ...tarea, estado: nuevoEstado } : tarea
+        )
+    }))
+    ,
+        
+    
     deleteTarea: (idTarea) => set((state) => ({
         tareas : state.tareas.filter((tarea) => tarea.id !== idTarea)
 
